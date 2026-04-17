ffa3a78ca8f86e21fe82aa92fb729dc046137a74
ramsaitejapelluri <ramsaitejapelluri@gamil.com>
2026-04-17 13:29:41 +0530

    add new file

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+# task 1
+for numbers in range(1,6):
+  print(numbers)
+print("--------------")
+
+#task 2
+for i in range(1,10,2):
+  print("even number:",i)
+print("----------------")
+#task 3
+for i in range(3):
+  for j in range(2):
+    print(i,j)
